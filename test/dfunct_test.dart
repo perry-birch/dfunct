b0a84b7a21a42f6840bc78b76e0a849100bf3512
Perry Birch <perrybirch@vizidrix.com>
2013-03-18 16:28:42 -0700

    Removed some silly testing code

--- test/dfunct_test.dart
+++ test/dfunct_test.dart
@@ -7,11 +7,6 @@ part 'src/monad_test.dart';
 part 'src/maybe_test.dart';
 
 void main() {
-  test('it', () {
-    expect(1, 1);
-    var a = new Maybe(10);
-  });
-
   monad_tests();
   maybe_tests();
 }
--- test/src/monad_test.dart
+++ test/src/monad_test.dart
@@ -3,24 +3,5 @@ part of dfunct_test;
 monad_tests() {
   group('-monad- should', () {
 
-    test('return ', () {
-
-      var func1 = (int a1, [int a2, int a3, int a4]) {
-        return a1 + a2 + a3 + a4;
-      };
-
-      var actual1 = func1(1, 2, 3, 4);
-
-      expect(actual1, 10);
-
-      //var func2 = func1(10);
-
-      //expect(func2 == null, false);
-      //var actual2 = func2(11);
-
-
-      expect(1, 1);
-    });
-
   });
 }
